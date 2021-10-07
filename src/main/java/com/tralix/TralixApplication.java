6413694f1c6fd8d70e582bc917da499aa883ca8a
Arthur <arturormzh1223@gmail.com>
2021-10-07 14:38:52 -0500

    Commit inicial

---
+++ src/main/java/com/tralix/TralixApplication.java
@@ -0,0 +1,13 @@
+package com.tralix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TralixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TralixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tralix/TralixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tralix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TralixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
