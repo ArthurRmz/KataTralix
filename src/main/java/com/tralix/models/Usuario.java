d676c23e17591ce326d1c1a13ac6c45e86fa91c7
Arthur <arturormzh1223@gmail.com>
2021-10-10 23:27:43 -0500

    Se agregan cambios modelo de usuarios, por errores

--- src/main/java/com/tralix/models/Usuario.java
+++ src/main/java/com/tralix/models/Usuario.java
@@ -1,9 +1,13 @@
 package com.tralix.models;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
 
@@ -12,11 +16,18 @@ import org.hibernate.validator.constraints.Email;
 import lombok.Data;
 
 @Data
-@Entity(name="usuarios")
-public class Usuario {
+@Entity
+@Table(name="usuarios")
+public class Usuario implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
-	@GeneratedValue
+	@Column(name = "id", unique = true, nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Email
